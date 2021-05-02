ac540e3f24801c62b2c7d9f2e6febf61824fafb4
Luke Harvey <luke.harvey@immixprotect.com>
2021-05-02 16:53:19 +0100

    Initial commit
    initial commit

---
+++ RPModServer/RPMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace RPModServer
+{
+    public class RPMain : BaseScript
+    {
+        public RPMain()
+        {
+        }
+
+    }
+}
